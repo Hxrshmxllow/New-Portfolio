b0cec27b12154eaaf18c7d96607a3ef3cbe05bf2
Hxrshmxllow <86836424+Hxrshmxllow@users.noreply.github.com>
2025-10-22 18:28:00 -0400

    updated live for noblemart

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -8,7 +8,7 @@ const projectData = [
     description:
       "A full-stack Spotify-style music platform built with Flask, React, and Firebase. Supports playlists, auth, and real-time playback.",
     tags: ["Python", "Flask", "React", "Firebase", "GCP"],
-    live: "https://hxrshmxllow.github.io/my-portfolio/#projects",
+    live: null,
     code: "https://github.com/Hxrshmxllow/Spotify",
   },
   {
@@ -16,7 +16,7 @@ const projectData = [
     description:
       "An AI-powered e-commerce site for fragrances with UPC-based inventory sync to Amazon & eBay.",
     tags: ["Python", "Flask", "React", "AWS", "DyanamoDB", "OpenAI API"],
-    live: "https://hxrshmxllow.github.io/my-portfolio/#projects",
+    live: "http://noblemart.s3-website-us-east-1.amazonaws.com/",
     code: "https://github.com/Hxrshmxllow/noblemart",
   },
   {
@@ -24,7 +24,7 @@ const projectData = [
     description:
       "An intelligent trading bot using Python, Alpaca API, and LSTM-based models for automated stock and options execution.",
     tags: ["Python", "TensorFlow", "Alpaca API"],
-    live: "https://hxrshmxllow.github.io/my-portfolio/#projects",
+    live: null,
     code: "https://github.com/Hxrshmxllow/Day-Trading-Bot",
   },
   {
@@ -32,14 +32,14 @@ const projectData = [
     description:
       "An AI-powered SAT prep platform that generates unlimited CollegeBoard-style questions for personalized and adaptive test practice.",
     tags: ["Python", "Flask", "React", "GCP", "Firebase", "Selenium"],
-    live: "https://hxrshmxllow.github.io/my-portfolio/#projects",
+    live: null,
     code: "https://github.com/Hxrshmxllow/PrepAI",
   },
   {
     title: "AI-Powered Chess Game",
     description: "An interactive chess platform supporting both player-vs-player matches and AI-powered gameplay for dynamic, adaptive challenges.",
     tags: ["Python", "Pytorch", "PyGame"],
-    live: "https://hxrshmxllow.github.io/my-portfolio/#projects",
+    live: null,
     code: "https://github.com/Hxrshmxllow/projects/",
   },
   {
@@ -47,35 +47,35 @@ const projectData = [
     tags: ["Python", "Flask", "React", "PyTorch"],
     description: "A full-stack poker platform featuring an AI-powered bot trained on OpenAI gameplay data for strategic, human-like decision-making.",
     code: "https://github.com/Hxrshmxllow/Projects/tree/main/Poker",
-    live: "https://hxrshmxllow.github.io/my-portfolio/#projects",
+    live: null,
   },
   {
     title: "NJ Transit Lost and Found",
     tags: ["Python", "Flask", "HTML", "CSS", "JavaScript"],
     description: "A web-based lost and found system for NJ Transit with Flask, OpenAI API, and a responsive frontend to help passengers easily report and recover lost items.",
     code: 'https://github.com/Hxrshmxllow/Projects/tree/main/NJ-Transit-Lost-and-Found',
-    live: "https://hxrshmxllow.github.io/my-portfolio/#projects",
+    live: null,
   },
   {
     title: "AI-Powered Tic-Tac-Toe",
     tags: ["Python", "Scikit-Learn"],
     description: "An intelligent Tic-Tac-Toe game powered by Scikit-Learn, training a model to predict optimal moves and adapt strategies based on real-time board states.",
     code: 'https://github.com/Hxrshmxllow/Projects/blob/main/Tic-Tac-Toe',
-    live: "https://hxrshmxllow.github.io/my-portfolio/#projects",
+    live: null,
   },
   {
     title: "Banking Application",
     tags: ["Python", "Flask", "HTML", "CSS", "SQLite"],
     description: "A full-stack banking platform featuring secure user accounts, seamless fund transfers, and a simulated money market for realistic financial interactions.",
     code: 'https://github.com/Hxrshmxllow/Projects/tree/main/Bank',
-    live: "https://hxrshmxllow.github.io/my-portfolio/#projects",
+    live: null,
   },
   {
     title: "RULate",
     tags: ["React-Native", "Firebase", "Google Maps API", "Stripe"],
     description: "A mobile Rutgers ride-sharing app designed to connect students and optimize routes for faster, more efficient campus travel.",
     code: 'https://github.com/Hxrshmxllow/HackRU',
-    live: "https://hxrshmxllow.github.io/my-portfolio/#projects",
+    live: null,
   }
 ];
 
@@ -91,7 +91,12 @@ export default function Projects() {
   const [redirectLink, setRedirectLink] = useState("https://hxrshmxllow.github.io/my-portfolio/#projects");
 
   const handleLiveClick = (url) => {
-    setShowPopup(true);
+    if(url){
+      window.open(url, "_blank", "noopener,noreferrer");
+    }
+    else{
+      setShowPopup(true);
+    }
   };
 
   const handleRedirect = () => {
@@ -125,7 +130,7 @@ export default function Projects() {
               <a
                 //href={proj.live}
                 className="btn primary"
-                onClick={(e) => handleLiveClick(e)}
+                onClick={(e) => handleLiveClick(proj.live)}
               >
                 Live Demo
               </a>
